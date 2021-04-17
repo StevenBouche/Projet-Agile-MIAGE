b35b38ec904611720ea8cc6fcd4b57599a01ec1e
Lina <lina.belkarfa@etu.unice.fr>
2021-04-17 15:03:28 +0200

    #25 UsersManager Fait

--- Microservices/UsersService/Services/UsersManager.cs
+++ Microservices/UsersService/Services/UsersManager.cs
@@ -28,8 +28,8 @@ namespace UsersService.Services
 
     public class UsersManager : IUsersManager
     {
-
         private readonly IMongoDBContext<Account> Context;
+        private object userAccount;
 
         public UsersManager(IMongoDBContext<Account> context)
         {
@@ -38,57 +38,73 @@ namespace UsersService.Services
 
         public Account CreateAccount(Account element)
         {
-            throw new NotImplementedException();
+            int nbAccountWithSameMail = this.Context.GetQueryable().Where(acc => acc.Email.Equals(element.Email)).Count();
+            if (nbAccountWithSameMail == 0)
+            {
+                this.Context.GetCollection().InsertOne(element);
+            }
+            return element;
         }
 
         public DeleteResult DeleteAccount(string id)
         {
-            throw new NotImplementedException();
+            return this.Context.GetCollection().DeleteOne(acc => acc.ID.Equals(id));
         }
 
         public Account GetAccountById(string element)
         {
-            throw new NotImplementedException();
+            return this.Context.GetQueryable().FirstOrDefault(account => account.ID.Equals(element));
         }
 
         public Account GetAccountWithRefreshToken(RefreshToken token)
         {
-            throw new NotImplementedException();
+            List<Account> list = this.Context.GetQueryable().ToList();
+            return list.FirstOrDefault(account => account.RefreshTokens.FirstOrDefault(refresh => refresh.Token.Equals(token.Token)) != null);
         }
 
         public Account GetAccountWithUserMail(string email)
         {
-            throw new NotImplementedException();
+            return this.Context.GetQueryable().FirstOrDefault(account => account.Email.Equals(email));
         }
 
         public List<Account> GetAllAccount()
         {
-            throw new NotImplementedException();
+            return this.Context.GetQueryable().ToList();
         }
 
         public List<AccountView> GetAllAccountView()
         {
-            throw new NotImplementedException();
+            return this.Context
+                            .GetQueryable()
+                            .Select(account => account.ToAccountView())
+                            .ToList();
         }
 
         public string GetPictureUser(string id)
         {
-            throw new NotImplementedException();
+            return this.Context.GetQueryable().FirstOrDefault(User => User.ID == id).Image;
         }
 
         public void SaveRefreshToken(Account userAccount)
         {
-            throw new NotImplementedException();
+            var update = Builders<Account>.Update.Set(account => account.RefreshTokens, userAccount.RefreshTokens);
+            this.Context.GetCollection().UpdateOne(account => account.ID.Equals(userAccount.ID), update);
         }
 
         public Account UpdateAccount(Account element)
         {
-            throw new NotImplementedException();
+            this.Context.GetCollection().ReplaceOne(acc => acc.ID.Equals(element.ID), element);
+            return element;
         }
 
         public void UpdateAccountFromView(AccountView element)
         {
-            throw new NotImplementedException();
+            Account a = this.GetAccountById(element.ID);
+
+            if (a is null) return;
+
+            a.UpdateFromView(element);
+            this.UpdateAccount(a);
         }
     }
 }
