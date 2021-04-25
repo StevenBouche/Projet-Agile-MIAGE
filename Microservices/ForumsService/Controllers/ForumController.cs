067df4bbaf33f0b7fc18fdd48dfb976b4f1ab7c6
Garnier <corentingarnier06@gmail.com>
2021-04-25 16:04:00 +0200

    #29 #30 #31 creation et gestion des Forums

--- Microservices/ForumsService/Controllers/ForumController.cs
+++ Microservices/ForumsService/Controllers/ForumController.cs
@@ -1,4 +1,5 @@
 ﻿using Authentification;
+using ForumServices.Models.View;
 using ForumsService.Services;
 using ForumsService.SignalR;
 using Microsoft.AspNetCore.Http;
@@ -17,7 +18,7 @@ namespace ForumsService.Controllers
     {
 
         IForumManagerView Manager;
-        private readonly IHubContext<ForumHub> HubContext;
+        private readonly IHubContext<ForumHub> Context;
         CacheUserWs Cache;
 
         private UserIdentity Identity
@@ -31,11 +32,53 @@ namespace ForumsService.Controllers
         public ForumController(IForumManagerView forumManager, IHubContext<ForumHub> hubContext, CacheUserWs cache)
         {
             this.Manager = forumManager;
-            this.HubContext = hubContext;
+            this.Context = hubContext;
             this.Cache = cache;
         }
 
+        [HttpGet("myforum")]
+        public ActionResult<List<ForumView>> GetMyForum()
+        {
+            List<ForumView> forums = this.Manager.GetForumsOfUser(this.Identity);
+            return this.Ok(forums);
+        }
 
+        [HttpPost("createForum")]
+        public async Task<ActionResult<ForumView>> CreateForum([FromBody] ForumForm value)
+        {
+            ForumView forum = this.Manager.CreateForum(value,this.Identity);
+            await this.Context.Clients.AllExcept(new string[] { Identity.ID }).SendAsync("onNewForum", forum);
+            return this.Ok(forum);
+        }
+
+        [HttpPost("searchForum")]
+        public ActionResult<ForumSearchView> SearchForums([FromBody] ForumSearchView search)
+        {
+            ForumSearchView searchResult = this.Manager.SearchForums(search, this.Identity);
+            return this.Ok(searchResult);
+        }
+
+        [HttpGet("subscribe/{id}")]
+        public ActionResult<SubscribeResultView> SubForum(string id)
+        {
+            SubscribeResultView result = this.Manager.UserSubscribe(id, this.Identity);
+            if (result.Result)
+            {
+                this.Context.Clients.AllExcept(new string[] { this.Identity.ID }).SendAsync("onUserSubscribe", result);
+            }
+            return this.Ok(result);
+        }
+
+        [HttpGet("panel/{id}")]
+        public ActionResult<string> GetForumPanel(string id)
+        {
+            ForumPanelView result = this.Manager.GetForumPanel(id, this.Identity);
+            result.Users.ForEach(user =>
+            {
+                user.IsConnected = this.Cache.usersIdWebSocket.Values.Contains(user.Id);
+            });
+            return this.Ok(result);
+        }
 
     }
 }
--- Microservices/ForumsService/Services/ForumManager.cs
+++ Microservices/ForumsService/Services/ForumManager.cs
@@ -45,52 +45,249 @@ namespace ForumsService.Services
 
         public ForumObj GetForumById(string id)
         {
-            throw new NotImplementedException();
+            return this.Context.GetQueryable().FirstOrDefault(forum => forum.Id.Equals(id));
         }
 
         public void AddChannelForum(string idForum, Channel channel, UserIdentity identity)
         {
-            throw new NotImplementedException();
+            ForumObj forum = this.GetForumById(idForum);
+
+            if ( forum == null )
+            {
+                return;
+            }
+
+            if ( forum.Users.Any(User => User.Id == identity.ID))
+            {
+                lock(LockObject)
+                {
+                    channel.Id = ObjectId.GenerateNewId().ToString();
+                    forum.Channels.Add(channel);
+                    this.Context.GetCollection().ReplaceOne(acc => acc.Id.Equals(forum.Id), forum);
+                }
+            }
         }
 
-        public void GetForumAndChannel(string idChannel, out ForumObj forum, out Channel channel, UserIdentity user)
+        public void GetForumAndChannel(string idChannel, out ForumObj forum, out Channel channel, UserIdentity identity)
         {
-            throw new NotImplementedException();
+            forum = null;
+            channel = null;
+
+            ForumObj resForum = this.Context.GetQueryable().FirstOrDefault(forum => forum.Channels.Where(channel => channel.Id == idChannel).Any());
+
+            if (resForum != null)
+            {
+                if(resForum.Users.Any(user => user.Id == identity.ID))
+                {
+                    forum = resForum;
+                    channel = resForum.Channels.FirstOrDefault(channel => channel.Id == idChannel);
+                }
+            }
         }
 
         public Message CreateAndAddNewMessage(string idforum, string idchannel, Message message, UserIdentity identity)
         {
-            throw new NotImplementedException();
+            bool permit = this.Context.GetQueryable()
+                .Any(Forum =>
+                        Forum.Id == idforum &&
+                        Forum.Users.Any(user => user.Id == identity.ID) &&
+                        Forum.Channels.Any(channel => channel.Id == idchannel));
+
+            if (permit && message.UserId == identity.ID)
+            {
+                lock (LockObject)
+                {
+                    message.Id = ObjectId.GenerateNewId().ToString();
+
+                    ForumObj forum = this.Context.GetQueryable().FirstOrDefault(f => f.Id == idforum && f.Channels.Any(channel => channel.Id == idchannel));
+
+                    forum.Channels.FirstOrDefault(channel => channel.Id == idchannel).Messages.Add(message);
+
+                    this.Context.GetCollection().ReplaceOne(f => f.Id == forum.Id, forum);
+
+                    return forum.Channels.FirstOrDefault(Channel => Channel.Id == idchannel).Messages.FirstOrDefault(m => m.Id == message.Id);
+
+                }
+
+            }
+
+            return message;
         }
 
         public bool RemoveChannelForum(string idForum, string idChannel, UserIdentity identity)
         {
-            throw new NotImplementedException();
+            bool permit = this.Context.GetQueryable()
+                .Any(Forum =>
+                        Forum.Id == idForum &&
+                        Forum.Users.Any(user => user.Id == identity.ID) &&
+                        Forum.Channels.Any(channel => channel.Id == idChannel));
+
+            if (permit)
+            {
+                lock (LockObject)
+                {
+                    ForumObj forum = this.Context.GetQueryable().FirstOrDefault(f => f.Id == idForum && f.Channels.Any(channel => channel.Id == idChannel));
+
+                    forum.Channels.RemoveAll(channel => channel.Id == idChannel);
+
+                    this.Context.GetCollection().ReplaceOne(f => f.Id == forum.Id, forum);
+
+                    var test = this.Context.GetQueryable()
+                    .Any(Forum =>
+                            Forum.Id == idForum &&
+                            Forum.Users.Any(user => user.Id == identity.ID) &&
+                            Forum.Channels.Any(channel => channel.Id == idChannel));
+
+                    return permit == true && test == false;
+                }
+                
+            }
+
+            return false;
         }
 
-        public ForumView CreateForum(ForumForm value, UserIdentity iD)
+        public ForumView CreateForum(ForumForm value, UserIdentity identity)
         {
-            throw new NotImplementedException();
+            var forum = new ForumObj
+            {
+                Id = ObjectId.GenerateNewId().ToString(),
+                Name = value.Name,
+                UrlPicture = value.Image,
+                Description = value.Description,
+                Channels = new List<Channel>(),
+                Users = new List<User>()
+            };
+
+            lock ( LockObject )
+            {
+                forum.Users.Add(new User
+                {
+                    Id = identity.ID,
+                    Pseudo = identity.Pseudo,
+                    UrlPicture = Config.URL + "/account/picture/" + identity.ID
+                });
+
+                this.Context.GetCollection().InsertOne(forum);
+            }
+
+            if (forum.Id != null)
+            {
+                return this.GetForumById(forum.Id).ToViewForum();
+            }
+
+            return forum.ToViewForum();
+
         }
 
         public ForumSearchView SearchForums(ForumSearchView search, UserIdentity iD)
         {
-            throw new NotImplementedException();
+            if(this.Context.GetQueryable().Count() >= 0)
+            {
+
+                List<ForumObj> forums = this.Context.GetQueryable().ToList();
+
+                forums.ForEach(forum =>
+                {
+                    forum.Users.ForEach(user =>
+                    {
+                        user.IsConnected = this.Cache.usersIdWebSocket.Values.Contains(user.Id);
+                    });
+                });
+
+                search.ForumSearch = forums.Select(element => new ForumView
+                {
+                    Id = element.Id,
+                    Name = element.Name,
+                    Description = element.Description,
+                    UrlPicture = element.UrlPicture,
+                    NbMember = element.Users.Count,
+                    NbOnline = element.Users.Where(user => user.IsConnected).Count()
+                }).ToList();
+
+                search.TotalItem = forums.Count;
+                
+            }
+
+            return search;
         }
 
-        public SubscribeResultView UserSubscribe(string idForum, UserIdentity iD)
+        public SubscribeResultView UserSubscribe(string idForum, UserIdentity identity)
         {
-            throw new NotImplementedException();
+            SubscribeResultView sub = new SubscribeResultView();
+            sub.Result = false;
+
+            if (String.IsNullOrEmpty(idForum))
+            {          
+                sub.Message = "id forum missing";
+                return sub;
+            }
+
+            if (String.IsNullOrEmpty(identity.ID))
+            {
+                sub.Message = "id user missing";
+                return sub;
+            }
+
+            lock (LockObject)
+            {
+                ForumObj forum = this.GetForumById(idForum);
+
+                if (forum == null)
+                {
+                    sub.Message = "forum not found";
+                    return sub;
+                }
+
+                if (forum.Users.Any(User => User.Id == identity.ID))
+                {
+                    sub.Message = "user is already subscribe";
+                    return sub;
+                }
+
+                User u = new User
+                {
+                    Id = identity.ID,
+                    Pseudo = identity.Pseudo,
+                    UrlPicture = identity.UrlPicture
+                };
+
+                forum.Users.Add(u);
+
+                this.Context.GetCollection().ReplaceOne((f => f.Id == forum.Id), forum);
+
+                sub.Result = true;
+                sub.IdForum = idForum;
+                sub.Message = "succes";
+                sub.User = u.ToUserView();
+                sub.User.IsConnected = this.Cache.usersIdWebSocket.Values.Contains(sub.User.Id);
+            }
+
+            return sub;
+
         }
 
         public List<ForumView> GetForumsOfUser(UserIdentity identity)
         {
-            throw new NotImplementedException();
+            return this.Context.GetQueryable()
+                .Where(forum => forum.Users.Where(user => user.Id == identity.ID).Any())
+                .ToList()
+                .Select(forum => forum.ToViewForum())
+                .ToList();
         }
 
         public ForumPanelView GetForumPanel(string id, UserIdentity identity)
         {
-            throw new NotImplementedException();
+            ForumObj forum = this.GetForumById(id);
+
+            ForumPanelView panel = new ForumPanelView();
+
+            if (forum == null) return panel;
+
+            panel.Forum = forum.ToViewForum();
+            panel.Channels = forum.Channels.Select(channel => channel.ToChannelView()).ToList();
+            panel.Users = forum.Users.Select(user => user.ToUserView()).ToList();
+
+            return panel;
         }
     }
 }
